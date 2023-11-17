db6832dd80d03acbc9b6b067a9af02908e99069d
Zuravvski <zurekseventyseven@gmail.com>
2023-11-17 22:42:06 +0100

    feat(DevOps): env setup

--- next.config.js
+++ next.config.js
@@ -1,15 +1,26 @@
+if (!URL.canParse(process.env.NEXT_PUBLIC_API_URL)) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables NEXT_PUBLIC_API_URL.
+  `);
+}
+
+const { protocol, hostname, port } = new URL(
+  process.env.NEXT_PUBLIC_API_URL
+);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'zuravvski.local',
-        port: '',
-        pathname: '/**'
-      }
-    ]
-  }
-}
+        protocol: protocol.slice(0, -1),
+        hostname,
+        port,
+        pathname: `/**`,
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
